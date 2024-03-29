5bb1c2411e75b10198e7a4e1e2a9c49aa0f9fe1a
bhaumik-2206 <123530037+BhaumikPanchal2206@users.noreply.github.com>
2024-03-29 18:32:54 +0530

    changes in dropdown

--- blog-page/src/components/dropdown-menu/index.jsx
+++ blog-page/src/components/dropdown-menu/index.jsx
@@ -12,15 +12,57 @@ const category = [
 ]
 
 const DropDownMenu = () => {
+    const [data, setData] = useState({ data: [], isOpen: false, index: -1 })
 
     const [isOpen, setIsOpen] = useState(false);
     const [subMenu, setSubMenu] = useState([]);
     const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
+    console.log(data)
 
     return (
         <>
             <div
+                onMouseLeave={() => {
+                    setData({ data: [], isOpen: false, index: -1 })
+                }}
+                className='relative z-10'>
+                <div className='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-3'>
+                    {category.map((ele, index) => (
+                        <button
+                            key={index}
+                            className="relative bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center"
+                            onMouseMove={() => {
+                                setData({ data: ele.items, isOpen: true, index: index });
+                            }}
+                        >
+                            <span>{ele.name}</span>
+                            {data.isOpen && data.index === index && (
+                                <div
+                                    onMouseLeave={() => {
+                                        setData({ data: [], isOpen: false, index: -1 })
+                                    }}
+                                    className="absolute left-0 top-full w-36 sm:w-48 bg-white rounded-md shadow-lg z-10">
+                                    <div className='py-1'>
+                                        {data.data.map((e, i) => (
+                                            <a key={i}
+
+                                                href={`#${e}`}
+                                                className="px-4 py-2 flex w-full justify-between text-gray-800 hover:bg-gray-200"
+                                            >
+                                                <p>{e}</p>
+                                                <i class="fa-solid fa-chevron-up fa-rotate-90"></i>
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </button>
+                    ))}
+                </div>
+            </div>
+
+            {/* <div
                 onMouseLeave={() => {
                     setIsOpen(false);
                 }}
@@ -47,6 +89,7 @@ const DropDownMenu = () => {
                     </svg>
                 </button>
 
+
                 <div className='flex absolute top-full'>
                     {isOpen && (
                         <div
@@ -70,83 +113,8 @@ const DropDownMenu = () => {
                             </div>
                         </div>
                     )}
-                    {isOpen && isSubMenuOpen && subMenu.length > 0 && (
-                        <div
-                            onMouseLeave={() => {
-                                setIsOpen(false)
-                                setSubMenu([]);
-                                setIsSubMenuOpen(false)
-                            }}
-                            onMouseEnter={() => {
-                                setIsOpen(true)
-                            }}
-                            className="mt-2 w-36 sm:w-48 bg-white rounded-md shadow-lg z-10">
-                            <div className='py-1'>
-                                {subMenu.map((e, i) => (
-                                    <a
-                                        key={i}
-                                        // onMouseEnter={() => }
-                                        // onMouseEnter={() => {
-                                        //     setSubMenu({ data: e.items, index: i })
-                                        //     setIsOpen(true)
-                                        // }}
-                                        href={`#${e}`}
-                                        className="px-4 py-2 flex w-full justify-between text-gray-800 hover:bg-gray-200"
-                                    >
-                                        <p>{e}</p>
-                                        <i class="fa-solid fa-chevron-up fa-rotate-90"></i>
-                                    </a>
-                                ))}
-                            </div>
-                        </div>
-                    )}
                 </div>
-
-
-                {/* {isOpen && (
-                    <>
-                        <div onMouseLeave={() => setIsOpen(false)} onMouseEnter={() => setIsOpen(true)} className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
-                            <div className='py-1'>
-                                {category.map((e, i) => (
-                                    <a key={i} onMouseEnter={() => {
-                                        console.log(e.items)
-                                        setSubMenu(e.items)
-                                    }
-                                    }
-                                        href="#"
-                                        className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
-                                    >
-                                        {Object.keys(e)[0]} <i class="fa-solid fa-chevron-up fa-rotate-90 ms-full"></i>
-                                    </a>
-                                ))}
-                            </div>
-                        </div>
-
-                        <div
-                            onMouseLeave={() => setIsOpen(false)}
-                            onMouseEnter={() => setIsOpen(true)}
-                            className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
-                            <div className='py-1'>
-                                {subMenu.data.map((e, i) => (
-                                    <a
-                                        // onMouseEnter={() => }
-                                        onMouseLeave={() => setSubMenu([])}
-                                        key={i}
-                                        // onMouseEnter={() => {
-                                        //     setSubMenu({ data: e.items, index: i })
-                                        //     setIsOpen(true)
-                                        // }}
-                                        href="#"
-                                        className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
-                                    >
-                                        {e} <i class="fa-solid fa-chevron-up fa-rotate-90 ms-full"></i>
-                                    </a>
-                                ))}
-                            </div>
-                        </div>
-                    </>
-                )} */}
-            </div>
+            </div> */}
         </>
     )
 }
--- blog-page/src/components/footer/index.jsx
+++ blog-page/src/components/footer/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
@@ -8,196 +7,117 @@ const Footer = () => {
                 <div className="w-[90vw] mx-auto border-t py-5">
                     <div className="mx-auto sm:px-4 ">
                         <div className="flex flex-wrap  items-center">
-                            <div className="lg:w-1/4 pr-4 pl-4 md:w-1/2 pr-4 pl-4 mb-4">
+                            <div className="lg:w-1/3 pr-4 pl-4 md:w-1/2 pr-4 pl-4 mb-4">
                                 <h1 className="mb-3 font-bold text-2xl">Teck Tonic</h1>
                                 <p>A108 Adam Street NY 535022, USA</p>
                                 <p className="m-0">Phone: +1 5589 55488 55</p>
                                 <p>Email: info@example.com</p>
-                                <Link to="/">
+                                <a href="/">
                                     <i
                                         style={{ cursor: "pointer" }}
                                         className="text-2xl bg-body-secondary text-black p-2 rounded-full fa-brands fa-twitter"
                                     />
-                                </Link>
-                                <Link to="/">
+                                </a>
+                                <a href="/">
                                     <i
                                         style={{ cursor: "pointer" }}
                                         className="text-2xl bg-body-secondary text-black p-2 rounded-full fa-brands fa-facebook"
                                     />
-                                </Link>
-                                <Link to="/">
+                                </a>
+                                <a href="/">
                                     <i
                                         style={{ cursor: "pointer" }}
                                         className="text-2xl bg-body-secondary text-black p-2 rounded-full fa-brands fa-instagram"
                                     />
-                                </Link>
-                                <Link to="/">
+                                </a>
+                                <a href="/">
                                     <i
                                         style={{ cursor: "pointer" }}
-                                        className="text-2xl bg-body-secondary text-black p-2 rounded-full fa-brands fa-linkedin"
+                                        className="text-2xl bg-body-secondary text-black p-2 rounded-full fa-brands fa-aedin"
                                     />
-                                </Link>
-                                <Link to="/">
+                                </a>
+                                <a href="/">
                                     <i
                                         style={{ cursor: "pointer" }}
                                         className="text-2xl bg-body-secondary text-black p-2 rounded-full fa-brands fa-facebook"
                                     />
-                                </Link>
+                                </a>
                             </div>
-                            <div className="lg:w-1/4 pr-4 pl-4 md:w-1/2 pr-4 pl-4 mb-4">
-                                <h3 className="mb-3">Useful Links</h3>
+                            <div className="lg:w-1/3 pr-4 pl-4 md:w-1/2 pr-4 pl-4 mb-4">
+                                <h3 className="mb-3">Useful as</h3>
                                 <ul className="list-inline">
                                     <li className="mb-2">
                                         <i
                                             style={{ fontSize: 10 }}
                                             className="fa-solid fa-chevron-right me-3 khakhi"
                                         />
-                                        <Link
+                                        <a
                                             style={{ textDecoration: "none" }}
                                             className="text-gray-900"
-                                            to="/"
+                                            href="#home"
                                         >
                                             Home
-                                        </Link>
+                                        </a>
                                     </li>
                                     <li className="mb-2">
                                         <i
                                             style={{ fontSize: 10 }}
                                             className="fa-solid fa-chevron-right me-3 khakhi"
                                         />
-                                        <Link
+                                        <a
                                             style={{ textDecoration: "none" }}
                                             className="text-gray-900"
-                                            to="/"
+                                            href="#about-us"
                                         >
                                             About Us
-                                        </Link>
+                                        </a>
                                     </li>
                                     <li className="mb-2">
                                         <i
                                             style={{ fontSize: 10 }}
                                             className="fa-solid fa-chevron-right me-3 khakhi"
                                         />
-                                        <Link
+                                        <a
                                             style={{ textDecoration: "none" }}
                                             className="text-gray-900"
-                                            to="/"
+                                            href="#blogs"
                                         >
-                                            Services
-                                        </Link>
+                                            Blogs
+                                        </a>
                                     </li>
                                     <li className="mb-2">
                                         <i
                                             style={{ fontSize: 10 }}
                                             className="fa-solid fa-chevron-right me-3 khakhi"
                                         />
-                                        <Link
+                                        <a
                                             style={{ textDecoration: "none" }}
                                             className="text-gray-900"
-                                            to="/"
+                                            href="#"
                                         >
                                             Termsof services
-                                        </Link>
+                                        </a>
                                     </li>
                                     <li className="mb-2">
                                         <i
                                             style={{ fontSize: 10 }}
                                             className="fa-solid fa-chevron-right me-3 khakhi"
                                         />
-                                        <Link
+                                        <a
                                             style={{ textDecoration: "none" }}
                                             className="text-gray-900"
-                                            to="/"
+                                            href="#"
                                         >
                                             Privacy policy
-                                        </Link>
+                                        </a>
                                     </li>
                                 </ul>
                             </div>
-                            <div className="lg:w-1/4 pr-4 pl-4 md:w-1/2 pr-4 pl-4 mb-4">
-                                <h3 className="mb-3">Our Servicrs</h3>
-                                <ul className="list-inline">
-                                    <li className="mb-2">
-                                        <i
-                                            style={{ fontSize: 10 }}
-                                            className="fa-solid fa-chevron-right me-3 khakhi"
-                                        />
-                                        <Link
-                                            style={{ textDecoration: "none" }}
-                                            className="text-gray-900"
-                                            to="/"
-                                        >
-                                            Web Design
-                                        </Link>
-                                    </li>
-                                    <li className="mb-2">
-                                        <i
-                                            style={{ fontSize: 10 }}
-                                            className="fa-solid fa-chevron-right me-3 khakhi"
-                                        />
-                                        <Link
-                                            style={{ textDecoration: "none" }}
-                                            className="text-gray-900"
-                                            to="/"
-                                        >
-                                            Web Development
-                                        </Link>
-                                    </li>
-                                    <li className="mb-2">
-                                        <i
-                                            style={{ fontSize: 10 }}
-                                            className="fa-solid fa-chevron-right me-3 khakhi"
-                                        />
-                                        <Link
-                                            style={{ textDecoration: "none" }}
-                                            className="text-gray-900"
-                                            to="/"
-                                        >
-                                            Product Management
-                                        </Link>
-                                    </li>
-                                    <li className="mb-2">
-                                        <i
-                                            style={{ fontSize: 10 }}
-                                            className="fa-solid fa-chevron-right me-3 khakhi"
-                                        />
-                                        <Link
-                                            style={{ textDecoration: "none" }}
-                                            className="text-gray-900"
-                                            to="/"
-                                        >
-                                            Marketing
-                                        </Link>
-                                    </li>
-                                    <li className="mb-2">
-                                        <i
-                                            style={{ fontSize: 10 }}
-                                            className="fa-solid fa-chevron-right me-3 khakhi"
-                                        />
-                                        <Link
-                                            style={{ textDecoration: "none" }}
-                                            className="text-gray-900"
-                                            to="/"
-                                        >
-                                            Graphic Design
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="lg:w-1/4 pr-4 pl-4 md:w-1/2 pr-4 pl-4 mb-4">
+                            <div className="lg:w-1/3 pr-4 pl-4 md:w-1/2 pr-4 pl-4 mb-4">
                                 <h3 className="mb-3">Our Newsletter</h3>
                                 <p>
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, nisi.
                                 </p>
-                                <div className="subscribe p-1 flex items-center bg-body-secondary justify-center rounded-full py-2 px-4 mt-4">
-                                    <input
-                                        type="text"
-                                        className="bg-transparent block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded sub border-0 rounded-full py-2 px-4"
-                                    />
-                                    <button className="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline text-gray-900 bg-khakhi rounded-full py-2 px-4">
-                                        Subscribe
-                                    </button>
-                                </div>
                             </div>
                         </div>
                     </div>
